a5268088e54fa2b0bbabda6daaf5a39d97062d17
masa2146 <bulutfatih551@gmail.com>
2021-03-03 22:33:29 +0300

    Created UI

--- custom.js
+++ custom.js
@@ -9,7 +9,53 @@ function deleteCity(id) {
   const request = new XMLHttpRequest();
   request.open("delete", CITY_API + "/" + id);
   request.setRequestHeader("Content-type", "application/json;charset=UTF-8");
-//   location.reload();
+  request.send();
+  request.onload = function () {
+    location.reload();
+  };
+}
+
+
+function showWeather(city) {
+  const request = new XMLHttpRequest();
+  request.open("get", WEATHER_API + "/" + city);
+  request.send();
+  request.onload = function () {
+    const json = JSON.parse(request.responseText);
+    if (json) {
+      document.getElementById("text-content").innerHTML =
+        json["name"] +
+        " is " +
+        json["weatherDescription"] +
+        "<br/>Temp      : " +
+        json["main"]["temp"] +
+        "<br/>Humidity  : " +
+        json["main"]["humidity"] +
+        "<br/>Pressure  : " +
+        json["main"]["pressure"];
+    }
+    $(".hover_bkgr_fricc").show();
+
+    $(".hover_bkgr_fricc").click(function () {
+      $(".hover_bkgr_fricc").hide();
+    });
+    $(".popupCloseButton").click(function () {
+      $(".hover_bkgr_fricc").hide();
+    });
+  };
+}
+
+function editCity(id) {
+  var city = document.getElementById("row_" + id).cells[1].innerHTML;
+  var country = document.getElementById("row_" + id).cells[2].innerHTML;
+  console.log({id:id, name: city, country: country });
+  const request = new XMLHttpRequest();
+  request.open("post", CITY_API + "/");
+  request.setRequestHeader("Content-type", "application/json;charset=UTF-8");
+  request.send(JSON.stringify({id:id, name: city, country: country }));
+  request.onload = function () {
+    location.reload();
+  };
 }
 
 function loadCities() {
@@ -23,24 +69,48 @@ function loadCities() {
       json.forEach((row) => {
         const tr = document.createElement("tr");
 
+        var id = -1;
+        var city = null;
         for (var cell in row) {
-          if (row === "id") {
-			const id = row[cell];
+          if (cell == "id") {
+            console.log(cell);
+            id = row[cell];
+          } else if (cell == "name") {
+            city = row[cell];
           }
           const td = document.createElement("td");
           td.textContent = row[cell];
+          td.contentEditable = "true";
           tr.appendChild(td);
         }
-        var element = document.createElement("input");
-        //Assign different attributes to the element.
-        element.type = "button";
-        element.value = "delete"; // Really? You want the default value to be the type string?
-        element.name = "delete"; // And the name too?
-        element.onclick = function () {
+        var deleteButton = document.createElement("input");
+        deleteButton.type = "button";
+        deleteButton.value = "Delete";
+        deleteButton.name = "delete";
+        deleteButton.onclick = function () {
           deleteCity(id);
         };
 
-        tr.appendChild(element);
+        var showWeatherButton = document.createElement("input");
+        showWeatherButton.type = "button";
+        showWeatherButton.value = "Show weather";
+        showWeatherButton.name = "weather";
+        showWeatherButton.onclick = function () {
+          showWeather(city.toLowerCase());
+        };
+
+        var editButton = document.createElement("input");
+        editButton.type = "button";
+        editButton.value = "Edit";
+        editButton.name = "edit";
+        editButton.onclick = function () {
+          editCity(id);
+        };
+
+        tr.id = "row_" + id;
+        tr.appendChild(deleteButton);
+        tr.appendChild(showWeatherButton);
+        tr.appendChild(editButton);
 
         const td = document.createElement("td");
         tr.appendChild(td);
@@ -70,7 +140,9 @@ function createCity() {
   request.open("post", CITY_API + "/");
   request.setRequestHeader("Content-type", "application/json;charset=UTF-8");
   request.send(JSON.stringify({ name: city, country: country }));
-  location.reload();
+  request.onload = function () {
+    location.reload();
+  };
 }
 
 document.addEventListener("DOMContentLoaded", () => {
